af4e4da28337783d5b6cc0918327b216e49ccab8
Guillaume <noel.guillaume@live.fr>
2017-10-01 00:54:03 +0200

    improved display

--- client/index.js
+++ client/index.js
@@ -10,25 +10,40 @@ window.onload = function() {
 
 			http.get(apiUrl+'/'+hashtag)
 			.then(function (response) {
-						console.log(response.data);
-						app.images = response.data;
-					})
+				console.log(response.data);
+				app.images = response.data;
+				window.img = response.data;
+			})
 			.catch(function (error) {
 				console.log(error);
 			});
 		}
 	}
 
+	const getClass = image => {
+		const now = new Date();
+		const imageDate = new Date(image.date);
+		let diff = new Date(now - imageDate);
+
+		console.log('diff', diff.getMinutes());
+
+		if (diff.getMinutes() < 1) return 'lessThan1';
+		if (diff.getMinutes() >= 15) return 'moreThan15';
+		return 'inBetween';
+	}
+
 	const app = new Vue({
 		el: "#twimg",
 		data: { 
-			hashtag: "",
+			hashtag: "#sneakers",
 			images: ""
 		},
 		methods: {
-			getImages: () => refreshImages()
+			getImages: refreshImages,
+			getClass: getClass
 		}
 	});	
 
+	app.getImages();
 	setInterval(refreshImages, 30000);
 };
